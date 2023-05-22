9d01a83a5e03fc5affba31928eb85ecdf4cc0f22
rakeshsk2122 <134278496+rakeshsk2122@users.noreply.github.com>
2023-05-22 16:31:19 +0530

    created areaofcircle and extension of  file
    prints area and extension after input given

---
+++ areaof circle.py
@@ -0,0 +1,6 @@
+r = 1.1
+print("the radius of the circle:")
+print(r)
+a = 3.14*r**2
+print("area of a circle with radius 1.1 :")
+print(a)
---
+++ filesearch.py
@@ -0,0 +1,11 @@
+
+x = "abc"
+y = ".py"
+print("file name:")
+print(x+y)
+if y == ".py":
+   print("the extension of file:")
+   print("'python'")
+else:
+   print("the extension of file:")
+   print("not python")
